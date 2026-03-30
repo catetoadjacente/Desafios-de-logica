f6d52a4423f440850f8c498f0cf50c6fec69a050
catetoadjacente <matheusaraujocunha.56@gmail.com>
2026-03-30 18:46:34 -0300

    n acabei

--- 13_e_o_pix_nada_ainda.py
+++ 13_e_o_pix_nada_ainda.py
@@ -3,3 +3,10 @@ Se ele trabalhar mais que 20 horas receberá um adicional de 30%, de hora extra.
 Objetivo: Desenvolva um sistema que calcule quanto este determinado competidor recebe ao fim do mês caso faça ou não horas extras no mês.
 Regras: O adicional se aplica apenas sobre as horas que superem as 20h regulares de trabalho.
 '''
+horas_trabalhadas = int(input('Digite quantas horas foram trabalhadas '))
+ganho = horas_trabalhadas * 60.20
+if horas_trabalhadas > 20:
+    hora_extra = horas_trabalhadas- 20
+    print('o competidor p, recebeu', (hora_extra*(30/100)))
+else:
+    print('o competidor p recebeu', ganho)     
